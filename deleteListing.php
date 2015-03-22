a6e7751bdfaf2a6031ae7fe4d4e65ce4eecc03ef
unknown <WHITEER2@BEUSCB2325.ds.sc.edu>
2015-03-22 17:49:59 -0400

    Updates to the delete listing and edit listing class

--- deleteListing.php
+++ deleteListing.php
@@ -1,6 +1,35 @@
 <?php
 
-//delete listing module for Ernest to code
+
+require_once 'Model/DB.php';
+
+$listingID = isset($_POST['listingID']);
+$userID = isset($_POST['userID']);
+
+if($listingID && $useriD){
+	
+	$listingID = $_POST['listingID'];
+	$userID = $_POST['userID'];
+	
+	$dbh = new DB();
+	
+	if($dbh->deleteListingByListingIDAndUserID($listingID, $userID)){
+		
+		echo 'listing deleted successfully';
+		
+	}
+	else{
+		
+		echo 'an error has occured';
+	}
+	
+}
+else
+	 {
+	
+	//do nothing
+	
+   }
 
 
 
--- editListing.php
+++ editListing.php
@@ -1,7 +1,97 @@
 <?php
+require_once 'Model/DB.php';
 
-//edit listing module for ernest to code
+$theUser = isset($_SESSION['user']);
+$price =  isset($_POST['price']);
+$isNegotiable = isset($_POST['isNegotiable']);
+$description = isset($_POST['description']);
+$listing = isset($_POST['listing']);
 
 
+	if($theUser)
+	{
 
-?>
+	$listingToUpdate = getListingbyListingID($listingID);
+
+	if(!$listingToUpdate){
+		
+	echo "listing doesn't exist";
+	
+	}
+	
+	else
+	{
+	
+		 if($price)
+	 	{
+	 		
+			$price = $_POST['price'];
+	 	
+	 	}
+		 else{
+		 	
+			$price = 0;
+			
+		 }
+		 
+	 
+		 if($isNegotiable)
+		 {
+		 	
+			$isNegotiable = $_POST['isNegotiable'];
+		
+	 	}
+		 else{
+		 	
+			$isNegotiable = 0;
+			
+		 }
+		 
+	 
+		 if($description)
+	 	{
+	 	
+		$description = $_POST['description'];
+		
+	 	}
+		 else{
+		 	
+		 	$description = "";
+			
+		 }
+	 
+	  if($listing)
+	 	{
+	 	
+		$listing = $_POST['listing'];
+		
+	 	}
+		 else{
+		 	
+		 	$listing = 0;
+			
+		 }
+	 
+	 	if(!$updateListing($listingToUpdate))
+	 	{
+	 	
+	 	echo 'could not update listing';
+	 
+		 }
+		
+	 	else 
+	 	{
+	 	
+		echo 'listing updated sucessfully';	
+		
+	 	}
+	 
+	}
+}// end of 
+	 else 
+		 {
+		 	echo 'lsisting updated sucessfully';
+		 }// end of else
+	 
+	 
+	 
