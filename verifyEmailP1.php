4bee43c0715965d76ebf1761786e6037a15e22e1
Bill <bglesias@gmail.com>
2015-03-23 14:22:23 -0400

    ssh test

--- verifyEmailP1.php
+++ verifyEmailP1.php
@@ -4,12 +4,14 @@ require_once 'Model/DB.php';
 require_once 'Model/Email.php';
 
 //uncomment for live purposes!
-$email = $_GET['email'];
+$email = isset($_GET['email']);
 
 //comment or delete for life purposes. This is for testing only
 //$email = 'glesias@email.sc.edu';
 
-if(isset($email)){
+if($email){
+	
+	$email = $_GET['email'];
 	
 	$newEmail = new Email();
 	
