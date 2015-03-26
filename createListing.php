7ed85386a340de8d8fa3c8a3104421804153aa3f
unknown <whiteer2@BEUSCB2482.ds.sc.edu>
2015-03-26 10:01:12 -0400

    clean up merge

--- createListing.php
+++ createListing.php
@@ -1,7 +1,25 @@
 <?php
+//set all fields 
+{
 
-//Module for Ernest to code
-//I ADDED A COMMENT TO DO A COMPARE AND PULL REQUEST. TAKE MY PULL REQUEST MAINE!
-//blahh hahahahahahahahah im sleepy
-//local comment change
+//if getbookby isbn
+	{
+		
+		
+		
+	
+		// if !getpublisherbypublishername
+		{
+			
+			
+			
+		}
+				
+	
+	
+	}
+	
+}
+
+
 ?>
