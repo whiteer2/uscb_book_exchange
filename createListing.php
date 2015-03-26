c5507c4dd3781d8f1e74c4ba2f5ee230875595ff
Ernest <whiteer2@email.sc.edu>
2015-03-26 13:46:32 -0400

    This is what i have thus far on my create listing .php file.....I

--- createListing.php
+++ createListing.php
@@ -1,25 +1,65 @@
 <?php
-//set all fields 
-{
+require_once 'Model/DB.php';
+$user = isset($_GET['ISBN']);
+$book = isset($_GET['author']);
+$publisher = isset($_GET ['publisherID']);
+$price = isset($_GET['price']);
+$description = isset($_GET['description']);
 
-//if getbookby isbn
+if(isLoggedIn($user))
+{
+	
+	
+	//check if both in db if not insert it
+
+	if(!getBookByISBN($ISBN))
 	{
 		
-		
-		
+	//if we can't get book, have to look for publisher
+	getPublisherIDByName($publisherName);
 	
-		// if !getpublisherbypublishername
+		if(!getPublisherIDByName($publisherName)){
+				
+			insertPublisher($publisherName);
+			
+			getPublisherIDByName($publisherName);
+		
+		}//end of if for !getPublisherIDByName($publisherName)
+		
+		}// if doesn't get ISBN
+
+	if(!getPublisherNameByName($publisherName))
+	
+	{
+		//
+		if(!insertPublisher($publisherName))
+		
 		{
 			
+		echo "error, could not create listing";	
 			
-			
-		}
-				
 	
-	
+		}	
 	}
 	
-}
-
-
+	
+	if(!createBook($book))
+	{
+		echo "error could not create listing";
+		
+		
+	}
+	else 
+	{
+		echo"error could not create listing ";
+	}
+	else
+	{
+		echo'error please fill out the form';
+	}
+	else
+		{
+			echo "please log in";
+		}
+			
 ?>
