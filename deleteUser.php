34c7c5e743d07b4e6599dbef67fd511daf905cdc
Zachary <adamszs@email.sc.edu>
2015-03-26 12:33:04 -0400

    module functions done

--- deleteUser.php
+++ deleteUser.php
@@ -1,6 +1,39 @@
 <?php
+require_once'Model/DB.php';
+require_once'Model/User.php';
 
 
 //delete user modulle for zach to code
-
+  
+  $deleteUser = isset($_POST['deleteUser']);
+  $user = isset($_SESSION['user']);
+  
+  
+  
+  if($user && $deleteUser)
+   {
+	
+	$deleteUser = $_POST['deleteUser'];
+ 	$user = $_SESSION['user'];
+	$userID = $user->getUserID();
+	   
+	if($deleteUser)
+	{
+		$dbh = new DB();
+		if($dbh->deleteUserByUserID($userID))
+		{
+			echo "deleted sucessfully";
+		}
+		else {
+			echo 'error has occured';
+		}
+	}
+	else {
+		echo "error has occured ";
+	}
+   }
+  else {
+    echo 'please log in';
+	 
+  }
 ?>
--- loginout.php
+++ loginout.php
@@ -1,6 +1,55 @@
 <?php
-
+include_once 'Model/DB.php';
+include_once 'Model/User.php';
+include_once 'Model/Password.php';
 
 //login/logout module for zach to code
+$email = isset($_POST['email']);
+$password = isset($_POST['password']);
+$logoutSet = isset($_POST['logout']);
+$logoutStatus = $_POST['logout'];
+
+
+if($email && $password)
+{
+	
+$email = $_POST['email'];
+$password = $_POST['password'];
+	
+ $pwHash = Password::hashPassword($password);
+ $user = $dbh->getUserByEmailID($emailID);
+ if(!$user)
+ {
+ 	echo 'incorrect email or password';
+	
+ }
+ else {
+ 	
+ 		$user = new User();
+	 
+     if($user->getPasswordHash() = $pwHash)
+	 {
+	 	
+	 	$user->login();
+		//redirect to html page
+		header("Location:http//www.uscbtextbookechange.com/Account");
+		
+	 }
+	 else {
+	 	
+		 echo 'incorrect user name and password';
+	 	}
+	 
+ }
+}
+else {
+		echo 'please put in a value for both username and password';
+}
+//LOGOUT CONTROLLER SECTION
+if($logoutSet && $logoutStatus )
+{
+	session_destroy();
+	head('Location:http://www.uscbtextbookexchange.com/Home');
+}
 
 ?>
