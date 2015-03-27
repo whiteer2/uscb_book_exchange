ea2860dee3933e1cfa055a107a92fc010fca72b9
Ernest <whiteer2@email.sc.edu>
2015-03-27 11:25:00 -0400

    changes for get all listing for bill to turn in final submition

--- getAllListingsForUser.php
+++ getAllListingsForUser.php
@@ -1,6 +1,37 @@
 <?php
+require_once 'Model/DB.php';
+$user = isset($_SESSION['user']);
+$listing = isset($_GET['listing']);
 
-//get all listing module for ernest to code
+if($user && $listing)
+{
 
+	$user = $_SESSION['user'];
+	
+	$result = getUserByUserID($userID);
+	
+	if(!result)
+	
+	{
+	
+		echo 'user has no listing or error';
+		
+	}// end of if statement
+	
+	else
+	
+	{
+		
+		echo 'json_eoncode(result)';
+		
+	}//end of else
+	
+}//end if statement
 
+else 
+{
+
+	echo 'user needs to log in to view this information';
+
+}
 ?>
