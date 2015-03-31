49ad876f665a80965c6bfff1bc53f376201901e2
sirTableFlipper <farristj@email.sc.edu>
2015-03-30 23:48:23 -0400

    3/30/15 11:49 PM

--- searchListings.php
+++ searchListings.php
@@ -1,12 +1,6 @@
 <?php
-//search listing module controller
-// Tremaine Farris
-// 3/21/2015
-
 require_once 'Model/theModel.php';
-
 $query = isset($_GET['query']);
-
 if($query){
 	
 	$query = $_GET['query'];
@@ -31,22 +25,4 @@ if($query){
 else{
 	
 }
-
->>>>>>> 1a1b1844b11029778e28ed1c8bb60f902b0a3189
-
-
-	if ($results)
-	{
-		echo json_encode(results);
-	} // end if
-	
-	else 
-	{
-		echo "We could not find what what you were looking for";
-	} // end if/else block
-} // end outer if	
-else 
-{
-		echo "Please enter something into the search box!";
-} // end if/else block
 ?>
